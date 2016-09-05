dc5295573f3e1e2816d44da3894e195fdb526ec2
ChimdinduDenalexOrakwue <denalexorakwue1@gmail.com>
2016-09-05 17:33:33 -0500

    update
    update

---
+++ ImageHandler.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 5, 2016
+
+@author: Denalex
+'''
+
+class ImageHandler(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
