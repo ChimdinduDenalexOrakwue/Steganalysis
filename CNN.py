7d8a049cfccea9ac19acc58179e527e96c1a4573
ChimdinduDenalexOrakwue <denalexorakwue1@gmail.com>
2016-10-08 16:07:01 -0500

    update
    update

--- CNN.py
+++ CNN.py
@@ -8,6 +8,8 @@ from __future__ import print_function
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
+import os
+#os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=gpu,floatX=float32"
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
