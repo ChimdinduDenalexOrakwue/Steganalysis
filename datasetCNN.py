5c3357234a60117ff3974e1c508c7ab28f9516ea
ep23934 <e.pineda@utexas.edu>
2016-11-08 16:08:07 -0600

    dense
    changed dense val

--- datasetCNN.py
+++ datasetCNN.py
@@ -28,7 +28,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(32))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
