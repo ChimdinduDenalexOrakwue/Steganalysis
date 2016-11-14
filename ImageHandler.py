75f62061cf90fe9020be2a2f2a598ecce17058e1
ChimdinduDenalexOrakwue <denalexorakwue1@gmail.com>
2016-11-14 00:00:11 -0600

    update

--- ImageHandler.py
+++ ImageHandler.py
@@ -12,22 +12,27 @@ from pygame import PixelArray
 
 class ImageHandler:
     
+    #initiate the image handler
     def __init__(self, path_name, save_path_name):
         self.path_name = str(path_name)
         self.save_path_name = save_path_name
         self.files = [f for f in listdir(self.path_name) if isfile(join(self.path_name, f))]
-        
+    
+    #set path in which the images are contained
     def setPath(self, path_name):
         self.path_name = path_name
         return
     
+    #set path to which data will be saved
     def setSavePath(self, save_path_name):
         self.save_path_name = save_path_name
         return
     
+    #update the list of files
     def update_files(self):
         self.files = [f for f in listdir(self.path_name) if isfile(join(self.path_name, f))]
-        
+    
+    #print all files in the directory
     def print_files(self):
         for i in range(0, len(self.files)):
             print(self.files[i])
@@ -54,6 +59,7 @@ class ImageHandler:
             im.save(self.save_path_name + image_name)
         return im
     
+    #generate noise map from a given image
     def get_noise_from_greyscale_noisemap(self, image_name = "", image_width = 28, image_height = 28):
         im = Image.open(image_name)
         im = im.convert('L')
@@ -74,6 +80,8 @@ class ImageHandler:
         maskIndex = 0
         sum = 0
         mask = [1.0, -2.0, 1.0, -2.0, 4.0, -2.0, 1.0, -2.0, 1.0]
+        
+        #applies the mask to the image (may be incorrect)
         for row in range(x - 1, x + 2):
             for column in range(y - 1, y + 2):
                 if self.in_bounds(row, column, image):
@@ -81,6 +89,7 @@ class ImageHandler:
                     maskIndex = maskIndex + 1
         return (sum / 9)
     
+    #checks if x and y are in range
     def in_bounds(self, x, y, image):
         return (x >= 0 and x < len(image)) and (y >=0 and y<len(image))
         
@@ -90,6 +99,7 @@ class ImageHandler:
     def get_segmented_noise_28(self, image_name):
         return
     
+    #gerate dataset of noise values
     def get_neural_input(self, classifier = 0):
         dataset = np.empty(shape=[0,1])
         self.update_files()
@@ -98,10 +108,12 @@ class ImageHandler:
             dataset = np.append(dataset, [[noise]], axis = 0)
         return dataset
     
+    #generate the noise map data set
     def get_neural_input_noisemap(self, classifier = 1):
         dataset = np.empty(shape=[28,28])
         self.update_files()
         for i in range(0, len(self.files)):
+            #get noise map and append it to list of noisemap (may be incorrect)
             noise = self.get_noise_from_greyscale_noisemap(image_name = self.path_name + "\\" + self.files[i])
             dataset = np.append(dataset, noise, axis = 0)
         return dataset
